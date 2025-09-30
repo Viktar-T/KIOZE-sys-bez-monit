6c045f64c08550581dd0652a58a9c0e143611f15
Viktar <taustyka.viktar@gmail.com>
2025-09-30 14:40:35 +0200

    fix 1

--- bezp-monit/docusaurus.config.js
+++ bezp-monit/docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'vtaustyka', // Usually your GitHub org/user name.
   projectName: 'KIOZE-sys-bez-monit', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
